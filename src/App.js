84ad89cc267cc5b541d28d9a3d653cdfeb42f78d
Miguel Malagon <miguelmalagon@outlook.com>
2019-07-24 20:37:05 -0500

    add libraries

--- src/App.js
+++ src/App.js
@@ -3,24 +3,71 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<main>
+				Main
+				<header className="header">
+					Header
+					<p>Lorem ipsum dolo</p>
+				</header>
+				<section className="hero">
+					Hero
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sumenda
+						potius quam expetenda. Nihil opus est exemplis hoc facere longius.
+					</p>
+				</section>
+				<div className="flex-container wrapper">
+					<section className="content">
+						Content
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sumenda
+							potius quam expetenda. Nihil opus est exemplis hoc facere longius.
+							At Zeno eum non beatum modo, sed etiam divitem dicere ausus est.
+							Nunc ita separantur, ut disiuncta sint, quo nihil potest esse
+							perversius. Itaque quantum adiit periculum! ad honestatem enim
+							illum omnem conatum suum referebat, non ad voluptatem. Ergo id est
+							convenienter naturae vivere, a natura discedere. Esse enim quam
+							vellet iniquus iustus poterat inpune. Tu enim ista lenius, hic
+							Stoicorum more nos vexat. Duo Reges: constructio interrete.
+						</p>
+						<p>
+							Quam ob rem tandem, inquit, non satisfacit? Haec quo modo
+							conveniant, non sane intellego. Ita ceterorum sententiis semotis
+							relinquitur non mihi cum Torquato, sed virtuti cum voluptate
+							certatio. Tubulum fuisse, qua illum, cuius is condemnatus est
+							rogatione, P. Quem ad modum quis ambulet, sedeat, qui ductus oris,
+							qui vultus in quoque sit? Miserum hominem! Si dolor summum malum
+							est, dici aliter non potest. Quo tandem modo? Ex rebus enim
+							timiditas, non ex vocabulis nascitur. Deinde prima illa, quae in
+							congressu solemus: Quid tu, inquit, huc? Quod eo liquidius faciet,
+							si perspexerit rerum inter eas verborumne sit controversia.
+						</p>
+					</section>
+
+					<aside className="sidebar">
+						Sidebar
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sumenda
+							potius quam expetenda. Nihil opus est exemplis hoc facere longius.
+							At Zeno eum non beatum modo, sed etiam divitem dicere ausus est.
+							Nunc ita separantur, ut disiuncta sint, quo nihil potest esse
+							perversius. Itaque quantum adiit periculum!
+						</p>
+					</aside>
+				</div>
+				<footer className="footer">
+					Footer
+					<p>
+						Esse enim quam vellet iniquus iustus poterat inpune. Tu enim ista
+						lenius, hic Stoicorum more nos vexat. Duo Reges: constructio
+						interrete.
+					</p>
+				</footer>
+			</main>
+		</div>
+	);
 }
 
 export default App;
